850d864ad6fa3fecfd3e3b8e07f0753e8aa37892
kana <chocolate.bar.favorite@gmail.com>
2024-04-12 10:01:21 +0900

    プロジェクトを初期化

---
+++ src/main/java/com/example/money_diary_service/MoneyDiaryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.money_diary_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyDiaryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyDiaryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/money_diary_service/MoneyDiaryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.money_diary_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyDiaryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
