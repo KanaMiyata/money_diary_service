123a946842705ddc32d7584492da1b1240e83be6
kana <chocolate.bar.favorite@gmail.com>
2024-04-16 14:34:56 +0900

    404を出力できるように設計

--- src/main/java/com/example/money_diary_service/MoneyDiaryMapper.java
+++ src/main/java/com/example/money_diary_service/MoneyDiaryMapper.java
@@ -3,10 +3,26 @@ package com.example.money_diary_service;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
+
 
 @Mapper
 public interface MoneyDiaryMapper {
+
     @Select("SELECT * FROM money_diaries")
     List<MoneyDiary> findAll();
+
+    @Select("SELECT * FROM money_diaries WHERE LIKE CONCAT(# {id})")
+    List<MoneyDiary> findByMoneyDiary(int id);
+
+    @Select("SELECT * FROM money_diaries WHERE id =#{id}")
+    Optional<MoneyDiary> findById(int id);
+
+    @Select("SELECT * FROM money_diaries WHERE TRANSACTION_TYPE =#{transactionType}")
+    List<MoneyDiary> findByTransactionType(String transactionType);
+
+    @Select("SELECT * FROM money_diaries WHERE DATE_ADDED BETWEEN #{startDate} AND #{endDate}")
+    List<MoneyDiary> findByDates(LocalDate startDate, LocalDate endDate);
 }
